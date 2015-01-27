5a10e74078ab030558bb6a13fc96aa109ae7fd66
Company <bing.yuan@163.com>
2015-01-27 11:08:57 +0800

    邀请码js

---
+++ WebAPI/Scripts/Main/Main.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    var code = getUrlParam("code");
+
+    if (code == "undefined" || code == "") {
+        $("#code").hide();
+    }
+    $("#code").text("您的邀请码：" + code);
+    $("#code").show();
+});
+
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
+}
